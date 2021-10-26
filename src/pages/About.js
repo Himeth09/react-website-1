eca8b76c117c1a72378dc1a7cfb51d3d499e2d32
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-10-26 16:15:59 -0400

    Added styling to About.css
    Added styling to About.css and added text to the About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import ProfileLogo from '../images/profile-logo.jpg';
+import ImageBanner from '../images/profile-logo.jpg';
 import '../styles/About.css'
 
 function About() {
     return (
         <div className="about">
-            <div className="about-container">
-                <div className="profile-pic">
-                    
-                </div>
+            <div className="about-top" style={{backgroundImage: `url(${ImageBanner})`}}>
+                
+            </div>
+            <div className="about-bottom">
                 <h1>About Me</h1>
+                <p>Junior at the University of North Carolina at Charlotte pursuing a Bachelor's in Computer Science with a concentration in Software Engineering.
+                Currently a TA for ITSC 3146 Intro to Operating Systems &amp; Networking. Been using a computer since elementary school, avid gamer and tech enthusiast, been known to tinker around with both hardware and software.
+                Windows 10, my pride and joy is my desktop that I built myself. Also loaded Windows 11 on my laptop that I take to class.
+                </p>
             </div>
         </div>
     )
