17092d430bc77a74ca2a1f064c7426995e21e192
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-10-26 14:40:40 -0400

    Created About.css
    Created About.css style page for the About page and added a div to contain the picture of myself that will be added later

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ProfileLogo from '../images/profile-logo.jpg';
+import '../styles/About.css'
 
 function About() {
     return (
         <div className="about">
             <div className="about-container">
+                <div className="profile-pic">
+                    
+                </div>
                 <h1>About Me</h1>
             </div>
         </div>
